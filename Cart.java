fdfbf130fcbcd766deed33d0d0035cd4bab5c706
Yushmithap <yushmitha6a1a@gmail.com>
2017-01-22 19:35:03 +0530

    Cart Back end

---
+++ Cart.java
@@ -0,0 +1,104 @@
+package com.niit.shopgirlbackend.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name="CART")
+@Component
+public class Cart implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private int id;
+	
+	@NotNull
+	private String userID;
+	
+	@NotNull
+	private String productName;
+	
+	@NotNull
+	@Column(length = 8, precision = 2)
+	private double price;
+	
+	@NotNull
+	private String status;
+	
+	@NotNull
+	private int quantity;
+	
+	
+	private Date dateAdded;
+	
+	@Id
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+
+	public void setDateAdded(Date dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+
+	
+	
+}
